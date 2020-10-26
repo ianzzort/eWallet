d3cb2a99fe29e9696f334aede52ee41fcd76c921
Arnoldo <arnol.ortizjs@gmail.com>
2020-10-26 08:30:53 -0600

    final

--- js/validacionLogin.js
+++ js/validacionLogin.js
@@ -1,30 +1,61 @@
+var errores;
+
 function iniciar(){
 	var btnenviar = document.getElementById("btnSend");
 	var form = document.getElementById("frmlogin");
+	var name = document.getElementById("txtNombre");
+	var apell = document.getElementById("txtApellido");
+	var contra = document.getElementById("txtContra");
+	var email = document.getElementById("txtCorreo");
+	var dui = document.getElementById("txtDui");
+	var nit = document.getElementById("txtNit");
+	var phone = document.getElementById("txtTelefono");
 
 	if(btnenviar.addEventListener){
 		btnenviar.addEventListener("click", function(){
 			//Validaciones (Función)
-			if (validarNombre(document.frmlogin.txtNombre.value) == 0 || validarNombre(document.frmlogin.txtApellido.value) == 0 ||
-				validarCorreo(document.frmlogin.txtCorreo.value) == 0 || validarDui(document.frmlogin.txtDui.value) == 0 ||
-				validarNit(document.frmlogin.txtNit.value) == 0 || validarTelefono(document.frmlogin.txtTelefono.value) == 0 ) {
-				form.action = "#";
+			errores = "";
+			if (validarNombre(name.value) == 0 || validarNombre(apell.value) == 0 ||
+				validarCorreo(email.value) == 0 || validarDui(dui.value) == 0 ||
+				validarNit(nit.value) == 0 || validarTelefono(phone.value) == 0 ) {
+					document.getElementById("fail").innerHTML = "<br><h3>Errores</h3> <p>"+ errores + "</p>";				
 			}else{
-				form.action = "./user/index.html";
+				guardarDatos(name.value, apell.vallue, contra.value, email.value, dui.value, nit.value, phone.value);
+				form.submit();
 			}
 		}, false);
 	}
 	else{
 		btnenviar.attachEvent("onclick", function(){
 			//Validaciones (Función)
-			if (validarNombre(document.frmlogin.txtNombre.value) == 0 || validarNombre(document.frmlogin.txtApellido.value) == 0 ||
-				validarCorreo(document.frmlogin.txtCorreo.value) == 0 || validarDui(document.frmlogin.txtDui.value) == 0 ||
-				validarNit(document.frmlogin.txtNit.value) == 0 || validarTelefono(document.frmlogin.txtTelefono.value) == 0 ) {
-				form.action = "#";
+			if (validarNombre(name.value) == 0 || validarNombre(apell.value) == 0 ||
+				validarCorreo(email.value) == 0 || validarDui(dui.value) == 0 ||
+				validarNit(nit.value) == 0 || validarTelefono(phone.value) == 0 ) {
+					document.getElementById("fail").innerHTML = "<br><h3>Errores</h3> <p>"+ errores + "</p>";
 			}else{
-				form.action = "./user/index.html";
+				guardarDatos(name.value, apell.vallue, contra.value, email.value, dui.value, nit.value, phone.value);
+				form.submit();
 			}
 		});
+	}
+}
+
+function guardarDatos(nombre, apellido, password, correo, dui, nit, telefono){
+	var Usuario = [
+		{'Nombre':nombre, 'Apellido':apellido, 'Contraseña': password, 'Email': correo, 'DUI': dui, 'NIT':nit, 'Telefono': telefono}
+	]
+
+	var recoveredData = localStorage.getItem('user')
+	if(recoveredData == null){
+	    //No tenemos nada guardado, por lo cual vamos a guardar el usuario
+	  localStorage.setItem('user', JSON.stringify(Usuario))
+	} else {
+	    //Tenemos algo, por lo cual vamos a añadir un nuevo usuario
+	  var data = JSON.parse(recoveredData)
+	  var newUser = {'Nombre':nombre, 'Apellido':apellido, 'Contraseña': password, 'Email': correo, 'DUI': dui, 'NIT':nit, 'Telefono': telefono}
+	  //Guardando nuevo usuario
+	  data.push(newUser)
+	  localStorage.setItem('user', JSON.stringify(data))
 	}
 }
 
@@ -33,7 +64,7 @@ function validarNombre(dato){
 	var valido = false;
 
 	if (dato == null || dato == "" || dato.length == 0) {
-        alert("No se ha ingresado ningún valor de nombre o apellido");
+        errores += "No se ha ingresado ningún valor de nombre o apellido";
         return 0;
     }
 
@@ -43,7 +74,7 @@ function validarNombre(dato){
     }
 
     if (valido == false) {
-        alert("Error en: " + dato + ". Debe ingresar 2 nombres y apellidos");
+        errores += "Error en: " + dato + ". Debe ingresar 2 nombres y apellidos";
         return 0;
     }
 
@@ -55,7 +86,7 @@ function validarCorreo(dato){
 	var valido = false;
 
 	if (dato == null || dato == "" || dato.length == 0) {
-        alert("No se ha ingresado ningún valor de correo");
+        errores = "No se ha ingresado ningún valor de correo";
         return 0;
     }
 
@@ -65,7 +96,7 @@ function validarCorreo(dato){
     }
 
     if (valido == false) {
-        alert("Su correo " + dato + " no es válido");
+        errores += "Su correo " + dato + " no es válido";
         return 0;
     }
 
@@ -77,7 +108,7 @@ function validarDui(dato){
 	var valido = false;
 
 	if (dato == null || dato == "" || dato.length == 0) {
-        alert("No se ha ingresado ningún valor de dui");
+        errores += "No se ha ingresado ningún valor de dui";
         return 0;
     }
 
@@ -87,7 +118,7 @@ function validarDui(dato){
     }
 
     if (valido == false) {
-        alert("Su número de Dui " + dato + " no es válido");
+        errores += "Su número de Dui " + dato + " no es válido";
         return 0;
     }
 
@@ -99,7 +130,7 @@ function validarNit(dato){
 	var valido = false;
 
 	if (dato == null || dato == "" || dato.length == 0) {
-        alert("No se ha ingresado ningún valor de nit");
+        errores += "No se ha ingresado ningún valor de nit";
         return 0;
     }
 
@@ -109,7 +140,7 @@ function validarNit(dato){
     }
 
     if (valido == false) {
-        alert("Su número de Nit " + dato + " no es válido");
+        errores += "Su número de Nit " + dato + " no es válido";
         return 0;
     }
 
@@ -121,7 +152,7 @@ function validarTelefono(dato){
 	var valido = false;
 
 	if (dato == null || dato == "" || dato.length == 0) {
-        alert("No se ha ingresado ningún valor de teléfono");
+        errores += "No se ha ingresado ningún valor de teléfono";
         return 0;
     }
 
@@ -131,7 +162,7 @@ function validarTelefono(dato){
     }
 
     if (valido == false) {
-        alert("Su número de teléfono " + dato + " no es válido");
+        errores += "Su número de teléfono " + dato + " no es válido";
         return 0;
     }
 
--- js/validarCuenta.js
+++ js/validarCuenta.js
@@ -1,26 +1,55 @@
+var errores;
+
 function iniciar(){
 	var btnenviar = document.getElementById("btnSend");
 	var form = document.getElementById("frmdatos");
+	var cantidad = document.getElementById("txtCantidad");
+	var acc = document.getElementById("txtCuenta");
+	var bank = document.getElementById("txtBanco");
 
 	if(btnenviar.addEventListener){
 		btnenviar.addEventListener("click", function(){
+			errores="";
 			//Validaciones (Función)
-			if (validarCantidad(document.frmdatos.txtCantidad.value) == 0 || validarCuenta(document.frmdatos.txtCuenta.value) == 0) {
-				form.action = "#";
+			if (validarCantidad(cantidad.value) == 0 || validarCuenta(acc.value) == 0) {
+				document.getElementById("error").innerHTML = "<p>"+ errores + "</p>";
 			}else{
-				form.action = "./panel.html";
+				guardarDatos(cantidad.value, bank.value, acc.value);
+				form.submit();
 			}
 		}, false);
 	}
 	else{
 		btnenviar.attachEvent("onclick", function(){
+			errores="";
 			//Validaciones (Función)
-			if (validarCantidad(document.frmdatos.txtCantidad.value) == 0 || validarCuenta(document.frmdatos.txtCuenta.value) == 0) {
-				form.action = "#";
+			if (validarCantidad(cantidad.value) == 0 || validarCuenta(acc.value) == 0) {
+				document.getElementById("error").innerHTML = "<br><p>"+ errores + "</p>";
 			}else{
-				form.action = "./panel.html";
+				guardarDatos(cantidad.value, bank.value, acc.value);
+				form.submit();
 			}
 		});
+	}
+}
+
+function guardarDatos(efectivo, banco, cuenta){
+	var usuario = JSON.parse(localStorage.getItem("user"));
+	var CuentaUsuario = [
+		{"Usuario": usuario, "Efectivo": efectivo, "Banco": banco, "Cuenta": cuenta}
+	]
+
+	var recoveredData = localStorage.getItem('account')
+	if(recoveredData == null){
+	    //No tenemos nada guardado, por lo cual vamos a guardar la cuenta
+	  localStorage.setItem('account', JSON.stringify(CuentaUsuario))
+	} else {
+	    //Tenemos algo, por lo cual vamos a añadir una nueva cuenta
+	  var data = JSON.parse(recoveredData)
+	  var newAcc = {"Usuario": usuario, "Efectivo": efectivo, "Banco": banco, "Cuenta": cuenta}
+	  //Guardando nueva cuenta
+	  data.push(newAcc)
+	  localStorage.setItem('account', JSON.stringify(data))
 	}
 }
 
@@ -29,7 +58,7 @@ function validarCantidad(dato){
 	var valido = false;
 
 	if (dato == null || dato == "" || dato.length == 0) {
-        alert("No se ha ingresado ningún valor saldo");
+        errores += "No se ha ingresado ningún valor saldo";
         return 0;
     }
 
@@ -39,7 +68,7 @@ function validarCantidad(dato){
     }
 
     if (valido == false) {
-        alert("Cantidad ingresada " + dato + " no válida");
+        errores += "Cantidad ingresada " + dato + " no válida";
         return 0;
     }
 
@@ -51,7 +80,7 @@ function validarCuenta(dato){
 	var valido = false;
 
 	if (dato == null || dato == "" || dato.length == 0) {
-        alert("No se ha ingresado ningún valor cuenta");
+        errores += "No se ha ingresado ningún valor cuenta";
         return 0;
     }
 
@@ -61,16 +90,33 @@ function validarCuenta(dato){
     }
 
     if (valido == false) {
-        alert("Su cuenta " + dato + " no es válido");
+        errores += "Su cuenta " + dato + " no es válido";
         return 0;
     }
 
     return 1;
 }
 
+function mostrarDatos(){
+	var i=0; 
+	var texto = document.getElementById("mostrar");
+	var usuario = JSON.parse(localStorage.getItem("user"));
+	for (var user in usuario){
+		i=user;
+	}
+
+	texto.innerHTML = "Bienvenido " + usuario[i].Nombre + "! Rellene la informacion solitada:";
+}
+
 if (window.addEventListener) {
-    window.addEventListener("load", iniciar, false);
+    window.addEventListener("load", function(){
+    	mostrarDatos();
+    	iniciar();
+    }, false);
 }
 else if (window.attachEvent) {
-    window.attachEvent("onload", iniciar);
+    window.attachEvent("onload", function(){
+    	mostrarDatos();
+    	iniciar();
+    });
 }
